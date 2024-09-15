0520d4edf73c0e27b952ecbe2aad3e581dba59da
saeed-mirjalili <saeedmirjalili1367@gmail.com>
2024-09-15 15:55:43 +0330

    continue

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('detail/<int:article_id>', views.detail, name='detail'),
     path('delete/<int:article_id>', views.delete, name='delete'),
-    path('create/', views.create, name='create')
+    path('create/', views.create, name='create'),
+    path('readpdf/<str:pdf_file>/<str:current_word>', views.find_next_word_in_pdf)
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
+import os
+from django.conf import settings
 from django.shortcuts import render, redirect
 from .models import Article
 from .forms import ArticleCreateForm
 from django.contrib import messages
+from PyPDF2 import PdfReader
 
 
 def home(request):
@@ -37,3 +40,39 @@ def create(request):
         form = ArticleCreateForm()
     return render(request, 'create.html', {'form':form})
 
+
+
+def find_next_word_in_pdf(request, pdf_file, current_word): 
+    pdf_path = os.path.join(settings.BASE_DIR, 'home', 'pdf', pdf_file + '.pdf') 
+
+    with open(pdf_path, 'rb') as file: 
+        pdf_reader = PdfReader(file)         
+        found_current_word = False 
+
+        for page_num in range(len(pdf_reader.pages)): 
+            page = pdf_reader.pages[page_num] 
+            text = page.extract_text() 
+            
+            if current_word in text: 
+                found_current_word = True 
+                
+                # تقسیم متن به جملات
+                sentences = text.split('\n')  # فرض بر این است که جملات با نقطه و فاصله جدا شده‌اند
+                
+                for i, sentence in enumerate(sentences):
+                    if current_word in sentence:
+                        # اگر جمله حاوی current_word پیدا شد، جمله بعدی را نمایش می‌دهیم
+                        if i + 1 < len(sentences):
+                            next_sentence = sentences[i + 1]
+                            messages.success(request, f'Title of your article is: {next_sentence}', 'success')
+                        else:
+                            messages.success(request, "No sentence found after the title.", 'danger')
+                        return redirect('home')
+                
+                # اگر کلمه در جمله‌ای پیدا نشد
+                messages.success(request, f"No sentence found containing '{current_word}' on page {page_num + 1}", 'danger') 
+                return redirect('home') 
+
+        if not found_current_word: 
+            messages.success(request, f"Word '{current_word}' not found in the PDF", 'danger') 
+            return redirect('home')
