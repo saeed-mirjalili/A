ba1cefe76d24d2f3e4abb05a7f57216e3a6790ef
saeed-mirjalili <saeedmirjalili1367@gmail.com>
2024-09-23 16:04:07 +0330

    Slight improvement in appearance

--- home/forms.py
+++ home/forms.py
@@ -10,7 +10,11 @@ class ArticleUploadForm(forms.Form):
 class ArticleReviewForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ['title', 'body']
+        fields = ['title', 'body', 'lang']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['lang'].widget = forms.HiddenInput()  # مخفی کردن فیلد lang
 
 class ArticleSearchForm(forms.Form):
     word = forms.CharField()
