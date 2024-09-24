1ad64b3ee02ab1d51a075d69bbe225cd8b93f02c
saeed-mirjalili <saeedmirjalili1367@gmail.com>
2024-09-24 14:41:59 +0330

    Start profiling

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,4 +3,9 @@ from django import forms
 class UserRegisterForm(forms.Form):
     username = forms.CharField()
     email = forms.CharField()
-    password = forms.FileField()
+    password = forms.CharField()
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
