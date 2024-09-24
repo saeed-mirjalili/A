fb61f2cb04421380fe7cf1def5e4110004e9d050
saeed-mirjalili <saeedmirjalili1367@gmail.com>
2024-09-24 14:41:59 +0330

    Start profiling

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,42 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserRegisterForm
+from .forms import UserLoginForm
+from django.contrib import messages
 
 def user_register(request):
-    user = User.objects.create_user('johna','john@infoa.com','123')
-    user.save()
+
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            messages.success(request, 'user registered successfully', 'success')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form':form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'You have successfully logged in')
+                return redirect('home')
+            else:
+                messages.error(request, 'username or password is wrong', 'danger')
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {'form':form})
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'you have successfully logged out', 'success')
     return redirect('home')
